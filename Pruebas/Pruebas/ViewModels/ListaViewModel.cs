00290e8897e78be8485594d81d64044dc91f4c21
Tavo Jmnz <tavo_x99@hotmail.com>
2017-09-02 09:26:16 -0500

    para github

---
+++ Pruebas/Pruebas/ViewModels/ListaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Pruebas.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas.ViewModels
+{
+    public class ListaViewModel
+    {        
+        public ObservableCollection<Persona> Personas { get; set; }
+
+        public ListaViewModel()
+        {
+            Personas = new ObservableCollection<Persona>();
+            for (int i = 0; i < 5; i++)
+            {
+                Personas.Add(new Persona()
+                {
+                    Nombre = $"Persona {i}",
+                    Pais = $"Pais {i}"
+                });
+            }
+        }
+    }
+}
---
+++ Pruebas/Pruebas/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Pruebas.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Pruebas.ViewModels
+{
+    public class MainViewModel: INotifyPropertyChanged
+    {
+        private string texto = "Elige algo";
+        public string Texto
+        {
+            set
+            {
+                if (texto != value)
+                {
+                    texto = value;
+                    PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("Texto"));
+                }
+            }
+            get
+            {
+                return texto;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand IrControles { get; }
+        public ICommand IrListView { get; }
+
+        public MainViewModel()
+        {
+            IrControles = new Command(NavegarMain);
+            IrListView = new Command(NavegarList);
+        }
+
+        private async void NavegarMain()
+        {            
+            await App.Current.MainPage.Navigation.PushAsync(new ControlesView());
+        }
+        private async void NavegarList()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new ListaView());
+        }
+    }
+}
